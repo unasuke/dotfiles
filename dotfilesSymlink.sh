cbb81cce375ef62a7a3a8685c99cfa2f555ffff6
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-04-04 10:30:19 +0900

    advice by neo6120 jigsaw thanks

--- dotfilesSymlink.sh
+++ dotfilesSymlink.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+cd $(dirname $0)
+for dotfile in .?*
+do
+	if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitmodules' ]
+	then
+		ln -Fis "$PWD/$dotfile" $HOME
+	fi
+done
 
+#by neo6120
+#thanks!
