1079dde7e1dc0f5ac7578b28efa957784cc10965
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-01-10 20:49:34 +0900

    Add annotation for install neobundle.vim in dotfilesSymlink.sh

--- dotfilesSymlink.sh
+++ dotfilesSymlink.sh
@@ -8,5 +8,9 @@ do
 	fi
 done
 
+echo "If it's first run, get neobundle.vim using git."
+echo "mkdir -p ~/.vim/bundle"
+echo "git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim"
+
 #by neo6120
 #thanks!
