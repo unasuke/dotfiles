20e1567060a64f1d0c44ff5c8409cca3749b9d20
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-11-08 22:22:18 +0900

    Change install method
    before use `ghq import`

--- other/repositories.sh
+++ other/repositories.sh
@@ -1,80 +1,80 @@
-git@github.com/ahomu/Talkie.git
-git@github.com/blockgiven/ruboty-gen.git
-git@github.com/capistrano/capistrano.git
-git@github.com/caskroom/homebrew-cask.git
-git@github.com/h2o/h2o.git
-git@github.com/harukasan/butter.git
-git@github.com/heroku/node-js-sample.git
-git@github.com/heroku/ruby-rails-sample.git
-git@github.com/hitode909/rokuga.git
-git@github.com/hshimoyama/xp_pair_programming.git
-git@github.com/ikegawa-koshi/idol-handshake.git
-git@github.com/kirikiriyamama/soror.git
-git@github.com/kosenconf-2015-tokyo/kosenconf.git
-git@github.com/mruby/mruby.git
-git@github.com/nikuzuki/QuizMaker.git
-git@github.com/nikuzuki/Taskapp.git
-git@github.com/nikuzuki/UnaEscape.git
-git@github.com/nikuzuki/demo_app3.git
-git@github.com/r7kamura/retro-twitter-client.git
-git@github.com/rails/rails.git
-git@github.com/rcmdnk/homebrew-file.git
-git@github.com/shinespark/electronic-facebook.git
-git@github.com/spice-life/deeplearning.git
-git@github.com/spice-life/kanoko.git
-git@github.com/spice-life/kanoko-app.git
-git@github.com/spice-life/miam.git
-git@github.com/spice-life/spice-life-puppet.git
-git@github.com/spice-life/spicelife-bot.git
-git@github.com/spice-life/spicelife-web.git
-git@github.com/spice-life/spotlights.git
-git@github.com/spice-life/steers.git
-git@github.com/spice-life/tmix-api.git
-git@github.com/spice-life/tmix-backend.git
-git@github.com/spice-life/tmix-manager.git
-git@github.com/spice-life/tmix-res.git
-git@github.com/spice-life/tmix-schema.git
-git@github.com/spice-life/tmix-web.git
-git@github.com/terakoya/rails.terakoya.io.git
-git@github.com/test-unit/test-unit.git
-git@github.com/test-unit/test-unit-rails.git
-git@github.com/twbs/bootstrap.git
-git@github.com/unasuke/AtCoder.git
-git@github.com/unasuke/blog.git
-git@github.com/unasuke/bookshelf_app.git
-git@github.com/unasuke/bootstrap.git
-git@github.com/unasuke/ceifpar.git
-git@github.com/unasuke/do-not-use-atom.git
-git@github.com/unasuke/dotfiles.git
-git@github.com/unasuke/esarea.git
-git@github.com/unasuke/hexapod-robot.git
-git@github.com/unasuke/homebrew-cask.git
-git@github.com/unasuke/homebrew-fonts.git
-git@github.com/unasuke/homebrew-miku.git
-git@github.com/unasuke/hubot.git
-git@github.com/unasuke/hubot-haruka.git
-git@github.com/unasuke/hubot-kotori.git
-git@github.com/unasuke/hubot-rin.git
-git@github.com/unasuke/idobata-hooks.git
-git@github.com/unasuke/idol-handshake.git
-git@github.com/unasuke/imas_api.git
-git@github.com/unasuke/jekyll-test.git
-git@github.com/unasuke/kosen10slt01-my-wip-projects.git
-git@github.com/unasuke/kosen10slt01-self-introduction.git
-git@github.com/unasuke/margarine.git
-git@github.com/unasuke/my-homebrew-setup.git
-git@github.com/unasuke/nerdtree-git-plugin.git
-git@github.com/unasuke/railbook.git
-git@github.com/unasuke/rails.terakoya.io.git
-git@github.com/unasuke/rails_demo_app.git
-git@github.com/unasuke/rails_sample_app.git
-git@github.com/unasuke/reveal.js.git
-git@github.com/unasuke/ruboty-shibuyarin.git
-git@github.com/unasuke/spina-test.git
-git@github.com/unasuke/ss.git
-git@github.com/unasuke/vim.git
-git@github.com/winebarrel/ridgepole.git
-git@github.com/yagi2/hubot-haruka.git
-git@github.com/yascentur/Ricty.git
-git@github.com/yuki24/rambulance.git
-git@git.yagi2.com:unasuke/itamae-recipes.git
+ghq get -p ahomu/Talkie
+ghq get -p blockgiven/ruboty-gen
+ghq get -p capistrano/capistrano
+ghq get -p caskroom/homebrew-cask
+ghq get -p h2o/h2o
+ghq get -p harukasan/butter
+ghq get -p heroku/node-js-sample
+ghq get -p heroku/ruby-rails-sample
+ghq get -p hitode909/rokuga
+ghq get -p hshimoyama/xp_pair_programming
+ghq get -p ikegawa-koshi/idol-handshake
+ghq get -p kirikiriyamama/soror
+ghq get -p kosenconf-2015-tokyo/kosenconf
+ghq get -p mruby/mruby
+ghq get -p nikuzuki/QuizMaker
+ghq get -p nikuzuki/Taskapp
+ghq get -p nikuzuki/UnaEscape
+ghq get -p nikuzuki/demo_app3
+ghq get -p r7kamura/retro-twitter-client
+ghq get -p rails/rails
+ghq get -p rcmdnk/homebrew-file
+ghq get -p shinespark/electronic-facebook
+ghq get -p spice-life/deeplearning
+ghq get -p spice-life/kanoko
+ghq get -p spice-life/kanoko-app
+ghq get -p spice-life/miam
+ghq get -p spice-life/spice-life-puppet
+ghq get -p spice-life/spicelife-bot
+ghq get -p spice-life/spicelife-web
+ghq get -p spice-life/spotlights
+ghq get -p spice-life/steers
+ghq get -p spice-life/tmix-api
+ghq get -p spice-life/tmix-backend
+ghq get -p spice-life/tmix-manager
+ghq get -p spice-life/tmix-res
+ghq get -p spice-life/tmix-schema
+ghq get -p spice-life/tmix-web
+ghq get -p terakoya/rails.terakoya.io
+ghq get -p test-unit/test-unit
+ghq get -p test-unit/test-unit-rails
+ghq get -p twbs/bootstrap
+ghq get -p unasuke/AtCoder
+ghq get -p unasuke/blog
+ghq get -p unasuke/bookshelf_app
+ghq get -p unasuke/bootstrap
+ghq get -p unasuke/ceifpar
+ghq get -p unasuke/do-not-use-atom
+ghq get -p unasuke/dotfiles
+ghq get -p unasuke/esarea
+ghq get -p unasuke/hexapod-robot
+ghq get -p unasuke/homebrew-cask
+ghq get -p unasuke/homebrew-fonts
+ghq get -p unasuke/homebrew-miku
+ghq get -p unasuke/hubot
+ghq get -p unasuke/hubot-haruka
+ghq get -p unasuke/hubot-kotori
+ghq get -p unasuke/hubot-rin
+ghq get -p unasuke/idobata-hooks
+ghq get -p unasuke/idol-handshake
+ghq get -p unasuke/imas_api
+ghq get -p unasuke/jekyll-test
+ghq get -p unasuke/kosen10slt01-my-wip-projects
+ghq get -p unasuke/kosen10slt01-self-introduction
+ghq get -p unasuke/margarine
+ghq get -p unasuke/my-homebrew-setup
+ghq get -p unasuke/nerdtree-git-plugin
+ghq get -p unasuke/railbook
+ghq get -p unasuke/rails.terakoya.io
+ghq get -p unasuke/rails_demo_app
+ghq get -p unasuke/rails_sample_app
+ghq get -p unasuke/reveal.js
+ghq get -p unasuke/ruboty-shibuyarin
+ghq get -p unasuke/spina-test
+ghq get -p unasuke/ss
+ghq get -p unasuke/vim
+ghq get -p winebarrel/ridgepole
+ghq get -p yagi2/hubot-haruka
+ghq get -p yascentur/Ricty
+ghq get -p yuki24/rambulance
+ghq get -p git@git.yagi2.com:unasuke/itamae-recipes.git
