f1078fa91a467d9a747cdeb33d0c0009266139de
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-11-27 17:29:44 +0900

    Add bundler aliases

--- zsh/.zsh.d/aliases.zsh
+++ zsh/.zsh.d/aliases.zsh
@@ -1,14 +1,16 @@
-####################
-##alias
-
+#alias
+#ls
 alias la='ls -alGh'
 alias ll='ls -lGh'
 
+#git
 alias g='git'
 compdef g=git
 
-##peco
-
+#peco
 #cd repository
 alias e='cd $(ghq list -p | peco)'
 
+#bundler
+alias bi='bundle install'
+alias be='bundle exec'
