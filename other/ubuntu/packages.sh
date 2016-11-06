fb34eac8ece6a5e837525653e9f0e48f1f48039e
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-11-06 23:44:43 +0900

    Packages for ubuntu

---
+++ other/ubuntu/packages.sh
@@ -0,0 +1,15 @@
+sudo apt update && sudo apt upgrade
+sudo apt install -y vim-nox \
+                    gpgv2 \
+                    tmux  \
+                    zsh \
+                    stow  \
+                    gcc \
+                    git-core  \
+                    libssl-dev  \
+                    libreadline-dev \
+                    zlib1g-dev  \
+                    xsel  \
+                    direnv  \
+                    tig \
+                    wine
