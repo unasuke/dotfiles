830cfe4beb424a41292e97199526432291b16848
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-01-30 21:21:58 +0900

    Long command notification

--- zsh/.zsh.d/darwin.zsh
+++ zsh/.zsh.d/darwin.zsh
@@ -9,3 +9,110 @@ bindkey '^r' peco-select-history
 alias la='ls -alGh'
 alias ll='ls -lGh'
 
+export PATH="/opt/confluent-6.1.0/bin:$PATH"
+export PATH="/opt/apache-maven-3.8.1/bin:$PATH"
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-15.0.1.jdk/Contents/Home
+#
+# https://qiita.com/hayamiz/items/d64730b61b7918fbb970
+autoload -U add-zsh-hook 2>/dev/null || return
+
+#
+# Notification of local host command
+# ----------------------------------
+#
+# Automatic notification via growlnotify / notify-send
+#
+#
+# Notification of remote host command
+# -----------------------------------
+#
+# "==ZSH LONGRUN COMMAND TRACKER==" is printed after long run command execution
+# You can utilize it as a trigger
+#
+# ## Example: iTerm2 trigger( http://qiita.com/yaotti/items/3764572ea1e1972ba928 )
+#
+#  * Trigger regex: ==ZSH LONGRUN COMMAND TRACKER==(.*)
+#  * Parameters: \1
+#
+
+__timetrack_threshold=20 # seconds
+read -r -d '' __timetrack_ignore_progs <<EOF
+less
+emacs vi vim
+ssh mosh telnet nc netcat
+gdb
+EOF
+
+export __timetrack_threshold
+export __timetrack_ignore_progs
+
+function __my_preexec_start_timetrack() {
+    local command=$1
+
+    export __timetrack_start=`date +%s`
+    export __timetrack_command="$command"
+}
+
+function __my_preexec_end_timetrack() {
+    local exec_time
+    local command=$__timetrack_command
+    local prog=$(echo $command|awk '{print $1}')
+    local notify_method
+    local message
+
+    export __timetrack_end=`date +%s`
+
+    if test -n "${REMOTEHOST}${SSH_CONNECTION}"; then
+        notify_method="remotehost"
+    elif which osascript >/dev/null 2>&1; then
+        notify_method="osascript"
+    elif which notify-send >/dev/null 2>&1; then
+        notify_method="notify-send"
+    else
+        return
+    fi
+
+    if [ -z "$__timetrack_start" ] || [ -z "$__timetrack_threshold" ]; then
+        return
+    fi
+
+    for ignore_prog in $(echo $__timetrack_ignore_progs); do
+        [ "$prog" = "$ignore_prog" ] && return
+    done
+
+    exec_time=$((__timetrack_end-__timetrack_start))
+    if [ -z "$command" ]; then
+        command="<UNKNOWN>"
+    fi
+
+    message="Command finished!\nTime: $exec_time seconds\nCOMMAND: $command"
+
+    if [ "$exec_time" -ge "$__timetrack_threshold" ]; then
+        case $notify_method in
+            "remotehost" )
+                # show trigger string
+                echo -e "\e[0;30m==ZSH LONGRUN COMMAND TRACKER==$(hostname -s): $command ($exec_time seconds)\e[m"
+                sleep 1
+                # wait 1 sec, and then delete trigger string
+                echo -e "\e[1A\e[2K"
+                ;;
+            "osascript" )
+                # echo "$message" | growlnotify -n "ZSH timetracker" --appIcon Terminal
+                osascript -e "display notification \"$message\" with title \"zsh\""
+                ;;
+            "notify-send" )
+                notify-send "ZSH timetracker" "$message"
+                ;;
+        esac
+    fi
+
+    unset __timetrack_start
+    unset __timetrack_command
+}
+
+if which osascript >/dev/null 2>&1 ||
+    which notify-send >/dev/null 2>&1 ||
+    test -n "${REMOTEHOST}${SSH_CONNECTION}"; then
+    add-zsh-hook preexec __my_preexec_start_timetrack
+    add-zsh-hook precmd __my_preexec_end_timetrack
+fi
