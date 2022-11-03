ffb45c7b13f6f255b667a6140b026a8c0eb5982f
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-11-03 20:23:25 +0900

    Create asdf plugin setup script (remove rbenv script)

--- other/ruby-versions.sh
+++
@@ -1,6 +0,0 @@
-rbenv install 2.2.7 &
-rbenv install 2.3.3 &
-rbenv install 2.4.1 &
-rbenv install 2.5.0-dev &
-
-wait
---
+++ other/setup_asdf.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+asdf plugin add ruby
+asdf plugin add rust
+asdf plugin add nodejs
+asdf plugin add python
+asdf plugin add golang
+asdf plugin add ghq
+asdf plugin add peco
+asdf plugin add delta
