46324d5d8c1e4065aa5f2bba5a3bb1fbd2ccbaaf
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-01-28 19:13:05 +0900

    1password ssh auth sock for macos

--- zsh/.zsh.d/darwin.zsh
+++ zsh/.zsh.d/darwin.zsh
@@ -126,3 +126,6 @@ if which osascript >/dev/null 2>&1 ||
     add-zsh-hook preexec __my_preexec_start_timetrack
     add-zsh-hook precmd __my_preexec_end_timetrack
 fi
+
+# https://developer.1password.com/docs/ssh/agent/compatibility/
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
