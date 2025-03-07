7dfce90ff20bf89705afaf91d95f1772756c87ec
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-03-07 10:44:23 +0900

    Unset TERMINFO when using ghostty
    go tools broken.

--- zsh/.zsh.d/darwin.zsh
+++ zsh/.zsh.d/darwin.zsh
@@ -126,3 +126,8 @@ fi
 
 # https://developer.1password.com/docs/ssh/agent/compatibility/
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
+# https://ghostty.org/docs/help/terminfo
+if [[ "$TERMINFO" == *Ghostty* ]] then
+  unset TERMINFO
+fi
